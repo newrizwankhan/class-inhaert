c0ac1750837bc608ea406ba39db00d0ef08cddec
newrizwankhan <patnul.rizwankhan@gmail.com>
2022-01-28 15:17:05 +0530

    class in

---
+++ shape.js
@@ -0,0 +1,16 @@
+// declare class
+class Shape {
+    constructor() {
+         this.color = "green";
+    }
+    drawShape() {
+
+    }
+    calculateArea() {
+
+    }
+}
+// export class using module.exports
+module.exports = Shape;
+© 2022 GitHub, Inc.
+Terms
